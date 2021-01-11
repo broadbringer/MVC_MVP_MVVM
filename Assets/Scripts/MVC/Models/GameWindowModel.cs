47e18877dbd71ad4a3ff8009049c57691508d840
Oleksandr Tokar <oleksandr.tokar@bini.games>
2021-01-11 19:26:41 +0300

    MVC pattern done

---
+++ Assets/Scripts/MVC/Models/GameWindowModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MVC.Views;
+
+namespace MVC.Models
+{
+    public class GameWindowModel
+    {
+        public event Action<int> DisplayedValueChanged;
+        public int DisplayedValue => displayedValue;
+        
+        private int displayedValue;
+
+        public GameWindowModel()
+        {
+            displayedValue = 0;
+        }
+
+        public void IncreaseValue(int value = 1)
+        {
+            displayedValue += value;
+            DisplayedValueChanged?.Invoke(displayedValue);
+        }
+        
+        public void DecreaseValue(int value = 1)
+        {
+            if (displayedValue <= 0) return;
+            
+            displayedValue -= value;
+            DisplayedValueChanged?.Invoke(displayedValue);
+        }
+
+        public void SetValueOnZero()
+        {
+            displayedValue = 0;
+            DisplayedValueChanged?.Invoke(displayedValue);
+        }
+    }
+}
---
+++ Assets/Scripts/MVC/Views/GameWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using MVC.Controllers;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MVC.Views
+{
+    public class GameWindow : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI displayedValue;
+        [SerializeField] private Button increasingValueButton;
+        [SerializeField] private Button decreasingValueButton;
+        [SerializeField] private Button clearingValueButton;
+        
+        public GameWindowController Controller { get; private set; }
+
+        
+        private void Start()
+        {
+            Controller = new GameWindowController(this);
+           
+            increasingValueButton.onClick.AddListener(Controller.IncreaseValue);
+            decreasingValueButton.onClick.AddListener(Controller.DecreaseValue);
+            clearingValueButton.onClick.AddListener(Controller.ClearValue);
+        }
+
+        public void DrawNewValue()
+        {
+            displayedValue.text = Controller.Model.DisplayedValue.ToString();
+        }
+    }
+}
